7f9b46799ae1326c77f663069223a4b51039bf80
edward <edwardthe3rdjnr@gmail.com>
2026-04-10 10:17:17 +0000

    Corrections

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,45 +1,52 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Stack, Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Platform } from 'react-native';
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Platform } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'dark'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Pending',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="tray.fill" color={color} />,
-        }}
+    <>
+      <Stack.Screen
+        options={{ title: "Mail Tracker", headerTitleAlign: "center" }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'History',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="clock.fill" color={color} />,
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? "dark"].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+          tabBarBackground: TabBarBackground,
+          tabBarStyle: Platform.select({
+            ios: { position: "absolute" },
+            default: {},
+          }),
         }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Pending",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="tray.fill" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: "History",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="clock.fill" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
